fc65d34694f38679775bdcd4cb3fda5ab851ec79
Matthew O'Neill <mjoneill1988@gmail.com>
2017-02-27 00:36:12 -0500

    First commit

---
+++ google_test.py
@@ -0,0 +1,30 @@
+def answer(s):
+    n = 2
+    new = [s[i:i+n] for i in range(0, len(s), n)]
+
+    for i in new:
+        if len(new[0]) == len(i):
+            equal_length = True
+        else:
+            equal_length = False
+
+    sorted_list = []
+    for i in new:
+        list_of = list(i) #elements to lists
+        sorted_element = sorted(list_of) #sort list
+        back_to_string = ''.join(sorted_element)#list back to string
+        sorted_list.append(back_to_string) #append
+
+    for i in sorted_list:
+        if i == sorted_list[0]:
+            equal_content = True
+        else:
+            equal_content = False
+
+    if equal_length == True and equal_content == True:
+        return len(sorted_list)
+    n += 1
+    return answer(s)
+
+l = answer('ababababababab')
+print(l)
